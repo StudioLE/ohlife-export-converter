88e0cf8104e9540bfd540a9555294786c9a1420b
Laurence Elsdon <git@elsdon.me>
2014-12-31 00:31:43 +0000

    Repackaged lib/out.js as sqwk

--- ohlife_export.js
+++ ohlife_export.js
@@ -1,36 +1,38 @@
 // Core modules
 var fs = require('fs')
 
+// Node modules
+var sqwk = require('sqwk')
+
 // App Modules
 var config = require('./config')
-var out = require('./lib/out')
 var title = require('./lib/title')
 var help = require('./lib/help')
 var open = require('./lib/open')
 
 // Send application title block to console
 title(function(err, messages) {
-	out.send(messages)
+	sqwk.send(messages)
 })
 
 // If no operation given then print help messages
 if(config.operation == undefined) {
 	help(function(err, messages) {
-		out.send(messages)
+		sqwk.send(messages)
 	})
 }
 // If the oh_life.txt file exists then open it
 else if(fs.existsSync(config.file)) {
 	open(config.file, config.operation, function(err, messages) {
 		if(err) {
-			out.error(err, true)
+			sqwk.error(err, true)
 		}
 		else {
-			out.send(messages)
+			sqwk.send(messages)
 		}
 	})
 }
 // If we can't find the file then 
 else {
-	out.error(['Could not locate OhLife file', config.file])
+	sqwk.error(['Could not locate OhLife file', config.file])
 }
